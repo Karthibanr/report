7aa3a36954ac3eb3464e98ee14c68f4b694551f1
Karthibanr <karthibanlink@gmail.com>
2025-03-20 18:10:46 +0530

    Update calculatePercentage.php

--- Trigger/calculatePercentage.php
+++ Trigger/calculatePercentage.php
@@ -10,49 +10,49 @@
 
     $conn = new mysqli($host, $user, $password, $database);
 
-    $batch = $_POST['batch'];
-    $department = $_POST['department'];
-    $institution = $_POST['institution'];
-    $graduation_year = $_POST['graduation_year'];
-    $section = $_POST['section'];
-    $programming = $_POST['programming'];
+    // $batch = $_POST['batch'];
+    // $department = $_POST['department'];
+    // $institution = $_POST['institution'];
+    // $graduation_year = $_POST['graduation_year'];
+    // $section = $_POST['section'];
+    // $programming = $_POST['programming'];
 
-    $whereClause = '';
+    // $whereClause = '';
     
-    if ($batch != "All") {
-        if ($whereClause == '') $whereClause = " WHERE batch = '".$batch."'";
-        else $whereClause .= " AND batch = '".$batch."'";
-    }
+    // if ($batch != "All") {
+    //     if ($whereClause == '') $whereClause = " WHERE batch = '".$batch."'";
+    //     else $whereClause .= " AND batch = '".$batch."'";
+    // }
 
-    if ($department != "All") {
-        if ($whereClause == '') $whereClause = " WHERE department = '".$department."'";
-        else $whereClause .= " AND department = '".$department."'";
-    }
+    // if ($department != "All") {
+    //     if ($whereClause == '') $whereClause = " WHERE department = '".$department."'";
+    //     else $whereClause .= " AND department = '".$department."'";
+    // }
 
-    if ($institution != "All") {
-        if ($whereClause == '') $whereClause = " WHERE institution = '".$institution."'";
-        else $whereClause .= " AND institution = '".$institution."'";
-    }
+    // if ($institution != "All") {
+    //     if ($whereClause == '') $whereClause = " WHERE institution = '".$institution."'";
+    //     else $whereClause .= " AND institution = '".$institution."'";
+    // }
 
-    if ($graduation_year != "All") {
-        if ($whereClause == '') $whereClause = " WHERE graduation_year = '".$graduation_year."'";
-        else $whereClause .= " AND graduation_year = '".$graduation_year."'";
-    }
+    // if ($graduation_year != "All") {
+    //     if ($whereClause == '') $whereClause = " WHERE graduation_year = '".$graduation_year."'";
+    //     else $whereClause .= " AND graduation_year = '".$graduation_year."'";
+    // }
 
-    if ($section != "All") {
-        if ($whereClause == '') $whereClause = " WHERE section = '".$section."'";
-        else $whereClause .= " AND section = '".$section."'";
-    }
+    // if ($section != "All") {
+    //     if ($whereClause == '') $whereClause = " WHERE section = '".$section."'";
+    //     else $whereClause .= " AND section = '".$section."'";
+    // }
 
-    if ($programming != "All") {
-        if ($whereClause == '') $whereClause = " WHERE programming = '".$programming."'";
-        else $whereClause .= " AND programming = '".$programming."'";
-    }
+    // if ($programming != "All") {
+    //     if ($whereClause == '') $whereClause = " WHERE programming = '".$programming."'";
+    //     else $whereClause .= " AND programming = '".$programming."'";
+    // }
 
     $studentListSql = "SELECT DISTINCT username, CONCAT(firstname, ' ', lastname) AS fullname, institution,
                         department, section, graduation_year, batch, programming 
                         FROM users";
-    $studentListSql .= $whereClause;
+    // $studentListSql .= $whereClause;
     
     $result = $conn->query($studentListSql);
 
@@ -504,5 +504,5 @@ foreach ($toopcourses as $course) {
 
 $output .= '</tbody></table></body></html>';
 
-echo $output;
+// echo $output;
 ?>
